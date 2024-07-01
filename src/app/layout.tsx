cde61c4df9dce2cfe1f5e10eec116a8bc9a6dd42
gabrcastro <gabrielcastromail@gmail.com>
2024-07-01 10:54:49 -0300

    add: framer motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import { ReactNode, createContext, useRef } from "react";
-
-// import { type CollapseMenuStore, createCollapseMenuStore } from '@/app/stores/store';
-
-// export type StoreApi = ReturnType<typeof createCollapseMenuStore>
-
-// export const StoreContext = createContext<StoreApi | undefined>(
-//   undefined,
-// )
-
-// export interface StoreProviderProps {
-//   children: ReactNode
-// }
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,11 +14,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  // const storeRef = useRef<StoreApi>()
-  // if (!storeRef.current) {
-  //   storeRef.current = createCollapseMenuStore()
-  // }
 
   return (
     <html lang="en">
--- src/app/view/components/sidebar_component/sidebar.component.tsx
+++ src/app/view/components/sidebar_component/sidebar.component.tsx
@@ -15,14 +15,27 @@ import {
 import CartItemComponent from "../cart_item_component/cart_item.component";
 import { ProductModel } from "@/app/domain/model/product.model";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function SidebarComponent() {
-  const { collapsed, toggleCollapsed, totalPrice, setTotalPrice, showModal, toggleModal, setCartTotal } = useStore();
-  const [uniqueProductsArray, setUniqueProductsArray] = useState<ProductModel[]>([]);
+  const {
+    collapsed,
+    toggleCollapsed,
+    totalPrice,
+    setTotalPrice,
+    showModal,
+    toggleModal,
+    setCartTotal,
+  } = useStore();
+  const [uniqueProductsArray, setUniqueProductsArray] = useState<
+    ProductModel[]
+  >([]);
 
   useEffect(() => {
     const storedProducts = localStorage.getItem("cart");
-    const productsArray: ProductModel[] = storedProducts ? JSON.parse(storedProducts) : [];
+    const productsArray: ProductModel[] = storedProducts
+      ? JSON.parse(storedProducts)
+      : [];
 
     let total = 0;
     productsArray.forEach((product) => {
@@ -34,32 +47,44 @@ export default function SidebarComponent() {
   }, []);
 
   return (
-    <Container>
-      <Header>
-        <Title>Carrinho de compras</Title>
-        <CloseButton onClick={toggleCollapsed}>
-          <CloseButtonText>X</CloseButtonText>
-        </CloseButton>
-      </Header>
+    <motion.div
+      initial={{ x: "100%" }} // Posição inicial fora da tela (à direita)
+      animate={{ x: 0 }} // Anima para a posição x = 0 (centro)
+      exit={{ x: "100%" }} // Animação de saída para fora da tela (à direita)
+      transition={{ type: "spring", stiffness: 120, damping: 15 }} // Transição suave usando spring
+      className="absolute top-0 right-0 h-full bg-white w-[400px] z-50 shadow-lg"
+    >
+      <Container>
+        <Header>
+          <Title>Carrinho de compras</Title>
+          <CloseButton onClick={toggleCollapsed}>
+            <CloseButtonText>X</CloseButtonText>
+          </CloseButton>
+        </Header>
 
-      <ProductsContainer>
-        {uniqueProductsArray.map((product) => (
-          <CartItemComponent key={product.id} product={product} />
-        ))}
-      </ProductsContainer>
+        <ProductsContainer>
+          {uniqueProductsArray.map((product) => (
+            <CartItemComponent key={product.id} product={product} />
+          ))}
+        </ProductsContainer>
 
-      <Footer>
-        <FooterRow>
-          <TotalText>Total</TotalText>
-          <TotalText>R${totalPrice.toFixed(2).toString()}</TotalText>
-        </FooterRow>
-        <BuyButton onClick={() => {
-          localStorage.removeItem("cart");
-          toggleCollapsed();
-          setCartTotal(0);
-          toggleModal(true);
-        }}>COMPRAR</BuyButton>
-      </Footer>
-    </Container>
+        <Footer>
+          <FooterRow>
+            <TotalText>Total</TotalText>
+            <TotalText>R${totalPrice.toFixed(2).toString()}</TotalText>
+          </FooterRow>
+          <BuyButton
+            onClick={() => {
+              localStorage.removeItem("cart");
+              toggleCollapsed();
+              setCartTotal(0);
+              toggleModal(true);
+            }}
+          >
+            COMPRAR
+          </BuyButton>
+        </Footer>
+      </Container>
+    </motion.div>
   );
 }
